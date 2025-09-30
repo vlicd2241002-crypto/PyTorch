346bac066bd53454aca5e3841ee42e137595224b
Lizeth <vlicd2241002@universidadmundomaya.edu.mx>
2025-09-29 21:04:19 -0600

    lectura

--- prog1.py
+++ prog1.py
@@ -1,2 +1,9 @@
 import torch
-import torch.nn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+import pandas as pd
+train = pd.read_csv("")
+print("x")
